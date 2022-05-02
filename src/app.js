eab3a302c34b8c3f86365e3bfb8b2580acb4b17b
moreira-edu <ml.ri7u@gmail.com>
2022-05-01 21:30:14 -0300

    rota drinks finalizada

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ import cors from "cors";
 import { moduloBebidas } from "./controller/BebidasController.js";
 import { moduloLanches } from "./controller/LanchesController.js";
 import { bannerErro, bannerMsg } from "./view/banner.js";
+import { moduloDrinks } from "./controller/DrinksController.js";
 const app = express();
 
 app.use(express.json(), cors());
 
-app.use(moduloBebidas, moduloLanches);
+app.use(moduloBebidas, moduloLanches, moduloDrinks);
 
 app.get("/", (req, res) => {
   res.send(bannerMsg("Porta Principal Funcionando"));
--- src/controller/DrinksController.js
+++ src/controller/DrinksController.js
@@ -1,11 +1,9 @@
-import { Drinks} from "../model/DrinksModel.js";
+import { Drinks } from "../model/DrinksModel.js";
 import { Router } from "../model/RouterModel.js";
 import { TabelaController } from "./TabelaController.js";
 
-
-
- export class DrinksController extends Drinks {
-       /**
+export class DrinksController extends Drinks {
+  /**
    * Construtor da classe DrinksController
    
    * @param {String} nome
@@ -13,79 +11,68 @@ import { TabelaController } from "./TabelaController.js";
    * @param {float} preco
    */
 
-constructor(nome, sabor, preco) {
+  constructor(nome, sabor, preco) {
     super(nome, sabor, preco);
 
-   this.Tabela = new TabelaController ("Drinks");
-    
-/**
- *
- * @instance
- * instancia da Classe Router
- */
-this.router = new Router("Drinks");
+    this.Tabela = new TabelaController("Drinks");
+
+    /**
+     *
+     * @instance
+     * instancia da Classe Router
+     */
+    this.router = new Router("Drinks");
+  }
 }
-}
-const drinks = new  DrinksController();
+const drinks = new DrinksController();
 
 drinks.router.get((req, res) => {
+  drinks.Tabela.requisitarTabela()
+    .then((data) => res.send({ Drinks: data }))
+    .catch((err) => {
+      res.status(404).send({ erro: err });
+    });
+});
 
-    drinks.Tabela.requisitarTabela()
-      .then((data) => res.send({ Drinks: data }))
-      .catch((err) => {
-        res.status(404).send({ erro: err });
-      });
+drinks.router.post((req, res) => {
+  drinks.Tabela.incluirDrink(req.body)
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(406).send(err);
+    });
+});
 
-  });
-
-  
- 
-
-  drinks.router.post((req, res) => {
-    
-   drinks.Tabela.incluirDrink(req.body) 
-      .then((data) => res.send(data))
-      .catch((err) => {
-        console.log(err);
-        res.status(406).send(err);
-      });
-      console.log(req)
-  }); 
-
- 
 drinks.router.filter("col", "valor", (req, res) => {
-    let coluna = req.params.col;
-    let valor = req.params.valor;
-   drinks.Tabela.filtraTabela(coluna, valor)
-      .then((data) => res.send(data))
-      .catch((err) => {
-        console.log(err);
-        res.status(404).send({erro: "Item não encontrado" });
-      });
-  });
+  let coluna = req.params.col;
+  let valor = req.params.valor;
+  drinks.Tabela.filtraTabela(coluna, valor)
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(404).send({ erro: "Item não encontrado" });
+    });
+});
 
-  
 drinks.router.getOnly("id", (req, res) => {
-   drinks.Tabela.requisitarItem(req.params.id)
-      .then((data) => res.send(data))
-      .catch((err) => {
-        res.status(404).send({ erro: "Item não encontrado" });
-        console.log(err);
-      });
-  });
- 
-  drinks.router.delete("id", (req, res) => {
-    drinks.Tabela.deletarItem(req.params.id)
-      .then((data) => res.send(data))
-      .catch((err) => console.log(err));
-  });
+  drinks.Tabela.requisitarItem(req.params.id)
+    .then((data) => res.send(data))
+    .catch((err) => {
+      res.status(404).send({ erro: "Item não encontrado" });
+      console.log(err);
+    });
+});
 
-  
-  drinks.router.put("id", (req, res) => {
-    drinks.Tabela.atualizarDrinks(req.params.id, req.body)
-      .then((data) => res.status(200).send(data))
-      .catch((err) => res.status(428).send(err));
-  });
-  
-  export const moduloDrinks = drinks.router.router;
-  
+drinks.router.delete("id", (req, res) => {
+  drinks.Tabela.deletarItem(req.params.id)
+    .then((data) => res.send(data))
+    .catch((err) => console.log(err));
+});
+
+drinks.router.put("id", (req, res) => {
+  drinks.Tabela.atualizarDrinks(req.params.id, req.body)
+    .then((data) => res.status(200).send(data))
+    .catch((err) => res.status(428).send(err));
+});
+
+export const moduloDrinks = drinks.router.router;
